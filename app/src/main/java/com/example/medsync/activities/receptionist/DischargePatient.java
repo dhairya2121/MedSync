a5eef1cc41f2cb86043db7030cd01b74e62df030
Dhairya <bt24cse070@iiitn.ac.in>
2026-04-15 14:53:38 +0530

    Discharge Patient activity

---
+++ app/src/main/java/com/example/medsync/activities/receptionist/DischargePatient.java
@@ -0,0 +1,19 @@
+package com.example.medsync.activities.receptionist;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.medsync.R;
+
+public class DischargePatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
